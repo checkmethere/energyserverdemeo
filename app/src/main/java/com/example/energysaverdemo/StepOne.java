eaeba43d44920aa2f853ee127248e0cacbf64982
checkmethere@gmail.com <Git2022Hub>
2022-11-19 23:12:25 +0200

    First Commit

---
+++ app/src/main/java/com/example/energysaverdemo/StepOne.java
@@ -0,0 +1,43 @@
+package com.example.energysaverdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class StepOne extends AppCompatActivity {
+    Button btnBackStepOne;
+    ImageView imageViewStepOne;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step_one);
+
+        btnBackStepOne = findViewById(R.id.btnBackStepOne);
+        imageViewStepOne =  findViewById(R.id.imageViewStepOne);
+        Intent intent = getIntent();
+
+        btnBackStepOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StepOne.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        imageViewStepOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StepOne.this,StepTwo.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+}
