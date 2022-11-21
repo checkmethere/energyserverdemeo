9e167cd24eb3243581a6d21831ef3ed9c01bce14
checkmethere@gmail.com <Git2022Hub>
2022-11-21 03:18:52 +0200

    Added

---
+++ app/src/main/java/com/example/energysaverdemo/StartupPage.java
@@ -0,0 +1,45 @@
+package com.example.energysaverdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class StartupPage extends AppCompatActivity {
+    Spinner spinner;
+    Button btnStart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup_page);
+
+        spinner = findViewById(R.id.spinnerLanguage);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.language_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        Intent intent = getIntent();
+        btnStart = findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartupPage.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+    }
+}
