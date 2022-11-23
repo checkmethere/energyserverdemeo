192d3a4859dc1d144178ceeb98b3e873bbf160b1
checkmethere@gmail.com <Git2022Hub>
2022-11-23 13:01:35 +0200

    Added method on video

--- app/src/main/java/com/example/energysaverdemo/VideoAnimation.java
+++ app/src/main/java/com/example/energysaverdemo/VideoAnimation.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.VideoView;
 
 public class VideoAnimation extends AppCompatActivity {
     ImageView imageViewAnimFwd;
+    VideoView videov;
+    Button clk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +28,14 @@ public class VideoAnimation extends AppCompatActivity {
                 finish();
             }
         });
+
+        videov = findViewById(R.id.videov);
+        clk = findViewById(R.id.clk);
+
+
+    }
+
+    public void videoplay(View view){
+
     }
 }
