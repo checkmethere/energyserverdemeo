10860c59f025444191831a62aedadf82ac70e020
checkmethere@gmail.com <Git2022Hub>
2022-11-25 00:58:25 +0200

    Added sound on all intents

--- app/src/main/java/com/example/energysaverdemo/VideoAnimation.java
+++ app/src/main/java/com/example/energysaverdemo/VideoAnimation.java
@@ -3,16 +3,20 @@ package com.example.energysaverdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class VideoAnimation extends AppCompatActivity {
     ImageView imageViewAnimFwd;
     VideoView videov;
     Button clk;
+    MediaController mediac;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +33,18 @@ public class VideoAnimation extends AppCompatActivity {
             }
         });
 
-        videov = findViewById(R.id.videov);
-        clk = findViewById(R.id.clk);
-
+//        videov = (VideoView) findViewById(R.id.videov);
+//        clk = (Button) findViewById(R.id.clk);
+//        mediac = new MediaController(this);
 
     }
 
     public void videoplay(View view){
-
+        String videopath = "android.resource://com.example.energysaverdemo"+R.raw.saveupvtwo;
+        Uri uri = Uri.parse(videopath);
+        videov.setVideoURI(uri);
+        videov.setMediaController(mediac);
+        mediac.setAnchorView(videov);
+        videov.start();
     }
 }
